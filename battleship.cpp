d10e36959051a84270f7d3df972570ade384eda3
SpasenovicBozidar <spasenovic2002@gmail.com>
2019-05-21 12:38:01 +0200

    Finish

--- battleship.cpp
+++ battleship.cpp
@@ -13,3 +13,61 @@
 #include <stdio.h>
 
 #include "battleship.h"
+
+FILE* my;
+FILE* op;
+static CellContent myField[FIELDSIZE][FIELDSIZE];//BUFFER
+static CellContent opField[FIELDSIZE][FIELDSIZE];//BUFFER
+static CellContent guess[FIELDSIZE][FIELDSIZE];//BUFFER
+
+void load_game(){
+  my = fopen("battleship.my", "r");
+  op = fopen("battleship.op", "r");
+
+  for (int i = 0; i < FIELDSIZE; ++i) {
+    for (int j = 0; j < FIELDSIZE; ++j) {
+      fread(&myField[i][j], sizeof(CellContent), 1, my);
+      fread(&opField[i][j], sizeof(CellContent), 1, op);
+      guess[i][j] = Unknown;
+    }
+  }
+  fclose(my);
+  fclose(op);
+}
+
+CellContent get_shot(int row, int col){
+  if (row> -1 && row< 10 && col>-1&&col<10)
+  {
+    return myField[row][col];
+  }
+  return OutOfRange;
+}
+
+
+bool shoot(int row, int col){
+  if(!(row> -1 && row< FIELDSIZE && col>-1&&col<FIELDSIZE ))
+    {
+      return false;
+}
+    for (int i = -1; i <= 1; ++i)
+    {
+        for (int j = -1; j <= 1; ++j)
+        {
+            if(row+i> -1 && row+i< FIELDSIZE && col+j>-1&&col+j<FIELDSIZE)
+            {
+              guess[row+i][col+j] = opField[row+i][col+j];
+            }
+
+        }
+    }
+     return true;
+}
+
+
+CellContent get_my_guess(int row, int col){
+  if (row> -1 && row< 10 && col>-1&&col<10)
+  {
+    return guess[row][col];
+  }
+return OutOfRange;
+}
